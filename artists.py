cb1b0a0bebf2c41a247546e6839e16c2bec17591
fonkvan <layedonovan@gmail.com>
2024-05-07 18:08:06 -0400

    Slightly modification
    Makes more sense for it to all be in one string rather than 2 and having to concatenate.

--- artists.py
+++ artists.py
@@ -3,7 +3,7 @@ import requests
 def get_artist(url, tokenDict):
     token = tokenDict["access_token"]
     auth = tokenDict["token_type"]
-    header = {"Authorization" : auth + f" {token}"}
+    header = {"Authorization" : f"{auth} {token}"}
     artist_dict = requests.get(url, headers=header)
     return artist_dict.json()
 
